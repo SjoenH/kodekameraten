7522110621361c870925a0881dcb1d475318d8f8
Henry S. Sjøen <henryskorpe@gmail.com>
2019-02-12 10:48:15 +0100

    rebuilding site Tue Feb 12 10:48:15 CET 2019

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# cd /Users/henry/git/kodekameraten
-
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
@@ -20,7 +18,6 @@ if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
-echo "${msg}"
 
 # Push source and build repos.
 msg = $(git push origin master)
